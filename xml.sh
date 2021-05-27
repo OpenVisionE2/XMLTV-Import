af2144b337ff9b31de8a3774fdb98c68f2a0cd4a
Persian Prince <persianpros@yahoo.com>
2021-05-27 08:09:24 +0430

    Update xml.sh

--- xml.sh
+++ xml.sh
@@ -4,6 +4,7 @@ cd etc/epgimport
 wget -O russian.sources.xml https://raw.githubusercontent.com/doglover3920/EPGimport-Sources/main/russian.sources.xml
 wget -O rytec.sources.xml https://raw.githubusercontent.com/doglover3920/EPGimport-Sources/main/rytec.sources.xml
 wget -O spainKoala.sources.xml https://raw.githubusercontent.com/doglover3920/EPGimport-Sources/main/spainKoala.sources.xml
+wget -O rytec.channels.xml https://raw.githubusercontent.com/doglover3920/EPGimport-Sources/main/rytec.channels.xml
 cd ..
 cd ..
 git add -u
