ed1d373adaf4b86b1a32852e9fb069c7a21e3065
persianpros <persianpros@yahoo.com>
2021-06-24 22:33:42 +0430

    Get rid of Travis forever as it's not free anymore!
    What travis-.com is advertising is bullshit!
    
    It's not free anymore not even for open source and you need to contact and beg them!
    
    They will give you some free credits then it finishes again so switch to github actions as soon as possible!

--- CI/xml.sh
+++ CI/xml.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cd etc/epgimport
+ls
 wget -O russian.sources.xml https://raw.githubusercontent.com/doglover3920/EPGimport-Sources/main/russian.sources.xml
 wget -O rytec.sources.xml https://raw.githubusercontent.com/doglover3920/EPGimport-Sources/main/rytec.sources.xml
 wget -O spainKoala.sources.xml https://raw.githubusercontent.com/doglover3920/EPGimport-Sources/main/spainKoala.sources.xml
@@ -9,4 +10,4 @@ cd ..
 cd ..
 git add -u
 git add *
-git commit -m "Fetch latest doglover3920's xml files. (build $TRAVIS_BUILD_NUMBER)"
+git commit -m "Fetch latest doglover3920's xml files."
