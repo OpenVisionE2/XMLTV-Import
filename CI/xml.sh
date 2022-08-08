c393de4c72671bb46819749316be853b0d2c2ab6
persianpros <persianpros@yahoo.com>
2022-08-09 00:38:25 +0430

    Support krkadoni sources

--- CI/xml.sh
+++ CI/xml.sh
@@ -6,8 +6,10 @@ wget -O russian.sources.xml https://raw.githubusercontent.com/doglover3920/EPGim
 wget -O rytec.sources.xml https://raw.githubusercontent.com/doglover3920/EPGimport-Sources/main/rytec.sources.xml
 wget -O spainKoala.sources.xml https://raw.githubusercontent.com/doglover3920/EPGimport-Sources/main/spainKoala.sources.xml
 wget -O rytec.channels.xml https://raw.githubusercontent.com/doglover3920/EPGimport-Sources/main/rytec.channels.xml
+wget -O krkadoni.channels.xml https://raw.githubusercontent.com/shaxxx/epg/main/output/krkadoni.channels.xml
+wget -O krkadoni.sources.xml https://raw.githubusercontent.com/shaxxx/epg/main/output/krkadoni.sources.xml
 cd ..
 cd ..
 git add -u
 git add *
-git commit -m "Fetch latest doglover3920's xml files."
+git commit -m "Fetch latest xml files."
